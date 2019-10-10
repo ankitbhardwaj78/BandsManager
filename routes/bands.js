aa4887225514c9a119bb73cd7b0109c07d3e9611
Ankit Bhardwaj <bhardwajankit438@gmail.com>
2019-10-10 12:56:47 +0530

    bands fetched and edit done

--- routes/bands.js
+++ routes/bands.js
@@ -1,9 +1,35 @@
 const Band = require('../models').Band
 const route = require('express').Router()
 
+
+route.get('/', (req, res) => {
+    if (req.session.user) {
+        Band.findAll({
+            where: {
+                userId: parseInt(req.session.user.id)
+            }
+        })
+            .then((bands) => {
+                res.status(200).send(bands);
+            })
+            .catch((err) => {
+                console.log(err);
+                req.session.user = null;
+                res.status(500).send({
+                    error: "some error occured"
+                })
+            });
+    }
+    else {
+        res.status(500).send({
+            error: "User not logged in"
+        })
+    }
+})
+
+
+
 route.post('/create', (req, res) => {
-    // We expect the req to have name,email,password,address,college,phn number in it
-    // We will create a new user 
     console.log("in post");
     console.log(req.body);
     Band.create({
@@ -21,5 +47,47 @@ route.post('/create', (req, res) => {
     })
 })
 
+
+route.delete('/delete', (req, res) => {
+    if (req.session.user) {
+        Band.destroy({
+            where: {
+                userId: req.session.user.id,
+                name: req.body.name,
+            }
+        })
+            .then((band) => {
+                res.send("removed successfully");
+            })
+            .catch((err) => {
+                res.send("some error occured!!!")
+            })
+    }
+    else {
+        res.status(501).send({
+            error: "user not logged in"
+        })
+    }
+})
+
+
+route.patch('/edit/:bandId', (req, res) => {
+    const updateOps = {};
+    updateOps["name"] = req.body.name;
+    Band.update(updateOps, { where: { id: req.params.bandId } })
+        .then(result => {
+            res.status(200).json({
+                message: "band updated",
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/bands/" + id
+                }
+            });
+
+        })
+        .catch(err => {
+            res.status(500).json({ error: "some error occured" });
+        });
+})
 
 exports = module.exports = route
